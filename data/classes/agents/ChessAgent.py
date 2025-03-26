711bd168da5b10a31b8dba9c7a6fe5ad6fd16890
Vivek Raman <vr.ac4bf@live.com>
2025-03-26 16:18:23 -0700

    Reformat code

--- data/classes/agents/ChessAgent.py
+++ data/classes/agents/ChessAgent.py
@@ -4,12 +4,13 @@ from typing import Literal
 from data.classes.Square import Square
 from data.classes.Board import Board
 
-class ChessAgent:
-    # Create private variables for Agent
-    def __init__(self, color: Literal['white', 'black']):
-        # In child classes, feel free to initialize any other state
-        # or helper functions
-        self.color = color
 
-    def choose_action(self, board: Board) -> tuple[Square, Square] | bool:
-        return False
+class ChessAgent:
+  # Create private variables for Agent
+  def __init__(self, color: Literal['white', 'black']):
+    # In child classes, feel free to initialize any other state
+    # or helper functions
+    self.color = color
+
+  def choose_action(self, board: Board) -> tuple[Square, Square] | bool:
+    return False
--- main.py
+++ main.py
@@ -5,20 +5,22 @@ from data.classes.agents.RandomPlayer import RandomPlayer
 from data.classes.agents.HumanPlayer import HumanPlayer
 from data.classes.agents.ChessAgent import ChessAgent
 
+
 def main():
-    parser = argparse.ArgumentParser(description="Initialize players for the game.")
-    parser.add_argument('white', type=str, help="Type of the white player")
-    parser.add_argument('black', type=str, help="type of the black player")
-    args = parser.parse_args()
-    if args.white not in globals().keys():
-        print(f'White player {args.white} not found!')
-        return
-    if args.black not in globals().keys():
-        print(f'Black player {args.black} not found!')
-        return
-    white_player: ChessAgent = globals()[args.white]('white')
-    black_player: ChessAgent = globals()[args.black]('black')
-    chess_match(white_player, black_player)
+  parser = argparse.ArgumentParser(description="Initialize players for the game.")
+  parser.add_argument('white', type=str, help="Type of the white player")
+  parser.add_argument('black', type=str, help="type of the black player")
+  args = parser.parse_args()
+  if args.white not in globals().keys():
+    print(f'White player {args.white} not found!')
+    return
+  if args.black not in globals().keys():
+    print(f'Black player {args.black} not found!')
+    return
+  white_player: ChessAgent = globals()[args.white]('white')
+  black_player: ChessAgent = globals()[args.black]('black')
+  chess_match(white_player, black_player)
+
 
 if __name__ == '__main__':
-    main()
+  main()
